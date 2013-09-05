efef27b92eadfa03525b744c5c18b04d2dcce66a
Edgar E. Iglesias <edgar.iglesias@gmail.com>
2013-09-05 23:35:49 +0200

    etrace: Declare protocol structs as packed
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@gmail.com>

--- etrace.h
+++ etrace.h
@@ -6,7 +6,7 @@ enum {
     TYPE_ARCH = 5,
     TYPE_BARRIER = 6,
     TYPE_INFO = 0x4554,
-};
+} __attribute__ ((packed)) ;
 
 struct etrace_hdr {
     uint16_t type;
@@ -14,7 +14,7 @@ struct etrace_hdr {
         uint16_t unit_id;
     };
     uint32_t len;
-};
+} __attribute__ ((packed));
 
 enum etrace_info_flags {
     ETRACE_INFO_F_TB_CHAINING   = (1 << 0),
@@ -26,7 +26,7 @@ struct etrace_info_data {
         uint16_t major;
         uint16_t minor;
     } version;
-};
+} __attribute__ ((packed));
 
 struct etrace_arch {
     struct {
@@ -34,17 +34,17 @@ struct etrace_arch {
         uint8_t arch_bits;
         uint8_t big_endian;
     } guest, host;
-};
+} __attribute__ ((packed));
 
 struct etrace_entry32 {
     uint32_t duration;
     uint32_t start, end;
-};
+} __attribute__ ((packed));
 
 struct etrace_entry64 {
     uint32_t duration;
     uint64_t start, end;
-};
+} __attribute__ ((packed));
 
 struct etrace_exec {
     uint64_t start_time;
@@ -52,12 +52,12 @@ struct etrace_exec {
         struct etrace_entry32 t32[0];
         struct etrace_entry64 t64[0];
     };
-};
+} __attribute__ ((packed));
 
 struct etrace_note {
     uint64_t time;
     uint8_t data8[0];
-};
+} __attribute__ ((packed));
 
 enum etrace_mem_attr {
     MEM_READ    = (0 << 0),
@@ -72,7 +72,7 @@ struct etrace_mem {
     uint32_t attr;
     uint8_t size;
     uint8_t padd[3];
-};
+} __attribute__ ((packed));
 
 struct etrace_tb {
     uint64_t vaddr;
@@ -81,7 +81,7 @@ struct etrace_tb {
     uint32_t guest_code_len;
     uint32_t host_code_len;
     uint8_t data8[0];
-};
+} __attribute__ ((packed));
 
 struct etrace_pkg {
 	struct etrace_hdr hdr;
